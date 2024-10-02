6092530089c7cd354cc3f22e5fd03664c6f854e4
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-10-02 09:55:40 +0200

    Kleur-01 Hoofdscherm

---
+++ git-taak-1-groep-abc-WannesPipien/git-taak-1-groep-abc-WannesPipien/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace git_taak_1_groep_abc_WannesPipien
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
